1df0270bea614b969d70817bb08792ad73bb72b8
Tina.Liang <teana0953@gmail.com>
2020-10-13 23:33:55 +0800

    Update component use vue-property-decorator way

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="home">
-        <img
-            alt="Vue logo"
-            src="../assets/logo.png"
-        >
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld :msg="msg" />
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Vue, Component } from 'vue-property-decorator';
+
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default Vue.extend({
-    name: 'Home',
+@Component({
     components: {
         HelloWorld,
     },
-});
+})
+class Home extends Vue {
+    private msg: string = 'Welcome to Your Vue.js + TypeScript App';
+}
+
+export default Home;
 </script>
