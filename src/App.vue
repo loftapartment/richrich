d0e9d628b0b3eab493efa5f6ae73aa246c0e85ba
Terry Liao <terry.liao@nogle.com>
2020-11-21 15:43:07 +0800

    add header links

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@
 
                 <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png" width="100" />
             </div>
-
             <v-spacer></v-spacer>
-
-            <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
+            <router-link to="/">
+                <v-btn color="white" text>Home</v-btn>
+            </router-link>
+            <router-link to="/about">
+                <v-btn color="white" text>About</v-btn>
+            </router-link>
         </v-app-bar>
 
         <v-main>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
 
 Vue.use(VueRouter);
 
